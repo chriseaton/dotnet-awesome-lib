1150490bd94635c9d6bd466c05b6c825a89ab8a6
Christopher Eaton <thedevexpert@gmail.com>
2016-11-08 14:01:29 -0800

    Added ModelState extension to facilitate generation of error information in a JSON friendly format.

---
+++ Awesome.Library.Mvc/ModelStateDictionaryExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Awesome.Library.Mvc {
+
+	public static class ModelStateDictionaryExtensions {
+
+		/// <summary>
+		/// Returns a simple string dictionary that lists model keys and errors.
+		/// </summary>
+		public static Dictionary<string, string[]> ToErrorDictionary( this ModelStateDictionary modelState ) {
+			return modelState.ToDictionary(
+				kvp => kvp.Key,
+				kvp => ( from n in kvp.Value.Errors
+						 where String.IsNullOrWhiteSpace( n.ErrorMessage ) == false
+						 select n.ErrorMessage ).ToArray()
+			);
+		}
+
+		/// <summary>
+		/// Returns a simple string dictionary that lists model keys and errors.
+		/// </summary>
+		public static Dictionary<string, string[]> ToJsonResult( this ModelStateDictionary modelState ) {
+			return new ModelStateJsonResult( modelState.ToDictionary(
+				kvp => kvp.Key,
+				kvp => ( from n in kvp.Value.Errors
+						 where String.IsNullOrWhiteSpace( n.ErrorMessage ) == false
+						 select n.ErrorMessage ).ToArray()
+			) );
+		}
+
+	}
+}
---
+++ Awesome.Library.Mvc/ModelStateJsonResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awesome.Library.Mvc {
+
+	[Serializable()]
+	public class ModelStateJsonResult : Dictionary<string, string[]> {
+
+		#region " Properties "
+
+		public bool Errors {
+			get { return this.Values.Any( a => a != null && a.Length > 0 ); }
+		}
+
+		#endregion
+
+		#region " Constructor(s) "
+
+		public ModelStateJsonResult() : base() { }
+
+		public ModelStateJsonResult( IDictionary<string, string[]> dictionary ) : base( dictionary ) { }
+
+		#endregion
+
+	}
+}
